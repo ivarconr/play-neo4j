6814d4d95c008c6b0b30fca517cdfd0642533a85
Ivar Østhus <ivarconr@gmail.com>
2014-02-19 21:18:06 +0100

    Added static resources

--- src/main/java/no/osthus/play/MyApplication.java
+++ src/main/java/no/osthus/play/MyApplication.java
@@ -1,9 +1,16 @@
 package no.osthus.play;
 
+import com.sun.jersey.spi.container.servlet.ServletContainer;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.ResourceHandler;
+import org.eclipse.jetty.servlet.FilterHolder;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.webapp.WebAppContext;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.ServletContext;
+import java.util.EnumSet;
 
 public class MyApplication {
 
@@ -14,19 +21,25 @@ public class MyApplication {
         }
 
         Server server = new Server(Integer.valueOf(webPort));
-        ServletContextHandler context = new ServletContextHandler();
+        WebAppContext context = new WebAppContext();
         context.setContextPath("/");
+        context.setResourceBase("src/main/webapp");
+        context.setWelcomeFiles(new String[]{"index.html"});
+
+
         server.setHandler(context);
 
-        ServletHolder jerseyServlet = context.addServlet(com.sun.jersey.spi.container.servlet.ServletContainer.class, "/*");
-        jerseyServlet.setInitOrder(1);
-        jerseyServlet.setInitParameter("com.sun.jersey.config.property.packages", "no.osthus.play");
-        jerseyServlet.setInitParameter("com.sun.jersey.api.json.POJOMappingFeature", "true");
-        jerseyServlet.setInitParameter("com.sun.jersey.config.property.WebPageContentRegex",
-                        "(/(image|js|css)/?.*)|"+
-                        "(/.*\\.html)|(/favicon\\.ico)|" +
-                        "(/robots\\.txt)");
+        //FilterHolder sh = new FilterHolder();
+        //sh.setFilter(new ServletContainer());
 
+        //context.addFilter(sh, "/rest/*", EnumSet.of(DispatcherType.REQUEST));
+        ServletHolder sh = context.addServlet(com.sun.jersey.spi.container.servlet.ServletContainer.class, "/rest/*");
+        sh.setInitParameter("com.sun.jersey.config.property.packages", "no.osthus.play");
+        sh.setInitParameter("com.sun.jersey.api.json.POJOMappingFeature", "true");
+        /*sh.setInitParameter("com.sun.jersey.config.property.WebPageContentRegex",
+                "(/(image|js|css)/?.*)|" +
+                        "(/.*\\.html)|(/favicon\\.ico)|" +
+                        "(/robots\\.txt)");*/
 
         try {
             server.start();
